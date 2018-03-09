8f657c80db2e70aeed2eb8ed5f17646312f4c5ce
Lambert <lambert.chen@oocl.com>
2018-03-09 11:57:38 +0800

    Refactoring: make it more S

--- src/PokerHand.py
+++ src/PokerHand.py
@@ -20,7 +20,8 @@ MAP_DICT = {"2": 15, "A": 14, "K": 13, "Q": 12, "J": 11, "10": 10, "9": 9, "8": 
             "3": 3}
 
 
-def is_straight(sorted_keys):
+def is_straight(point_count):
+    sorted_keys = sorted(point_count.keys(), key=lambda point: MAP_DICT[point])
     if sorted_keys == ['3', '4', '5', 'A', '2']:
         return True
     return MAP_DICT[sorted_keys[4]] == MAP_DICT[sorted_keys[3]] + 1 == MAP_DICT[sorted_keys[2]] + 2 == MAP_DICT[
@@ -43,13 +44,12 @@ def get_category(card1, card2, card3, card4, card5):
     )
     point_count = Counter(all_card_points)
     max_times_of_point = max(list(point_count.values()))
-    sorted_keys = sorted(point_count.keys(), key=lambda point: MAP_DICT[point])
     if is_all_card_in_same_suit(card1, card2, card3, card4, card5):
-        if is_straight(sorted_keys):
+        if is_straight(point_count):
             return ROYAL_FLUSH
         return FLUSH
     if max_times_of_point == SINGLE_CARD_POINT:
-        if is_straight(sorted_keys):
+        if is_straight(point_count):
             return STRAIGHT
         return HIGH_CARD
     if max_times_of_point == PAIR_CARD_POINT:
